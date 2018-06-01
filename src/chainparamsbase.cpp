748ea976bdf0a82a10a600d67cd361d7addd6b22
foo87b <foo87b@users.noreply.github.com>
2018-06-02 00:44:57 +0900

    change network parameters

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,11 +33,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 8332);
+        return MakeUnique<CBaseChainParams>("", 8762);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet", 18332);
+        return MakeUnique<CBaseChainParams>("testnet", 18798);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 18443);
+        return MakeUnique<CBaseChainParams>("regtest", 28762);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
