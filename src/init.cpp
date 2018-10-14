ff288f1282135c23b082fc036e5c684dfc0cd84e
serisia <serisia.gma@gmail.com>
2018-10-14 22:51:12 +0900

    change url bitcoin to xpchain

--- src/init.cpp
+++ src/init.cpp
@@ -527,8 +527,8 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoin/bitcoin>";
-    const std::string URL_WEBSITE = "<https://bitcoincore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/xpc-wg/xpchain>";
+    const std::string URL_WEBSITE = "<https://www.xpchain.io/>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
