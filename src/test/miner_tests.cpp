21cc330933a177513351f2c8104509e307f1971f
mban <mban259@users.noreply.github.com>
2018-10-28 07:11:02 +0900

    change check height

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -367,8 +367,8 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 
     // subsidy changing
     int nHeight = chainActive.Height();
-    // Create an actual 209999-long block chain (without valid blocks).
-    while (chainActive.Tip()->nHeight < 209999) {
+    // Create an actual 9999-long block chain (without valid blocks).
+    while (chainActive.Tip()->nHeight < 9999) {
         CBlockIndex* prev = chainActive.Tip();
         CBlockIndex* next = new CBlockIndex();
         next->phashBlock = new uint256(InsecureRand256());
@@ -379,8 +379,8 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
         chainActive.SetTip(next);
     }
     BOOST_CHECK(pblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
-    // Extend to a 210000-long block chain.
-    while (chainActive.Tip()->nHeight < 210000) {
+    // Extend to a 10000-long block chain.
+    while (chainActive.Tip()->nHeight < 10000) {
         CBlockIndex* prev = chainActive.Tip();
         CBlockIndex* next = new CBlockIndex();
         next->phashBlock = new uint256(InsecureRand256());
