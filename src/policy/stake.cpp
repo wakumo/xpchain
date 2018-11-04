555ce1cf87bf4274489c3adf62707888f421c685
mban <mban259@users.noreply.github.com>
2018-11-05 02:22:09 +0900

    remove function not used change #ifndef XPCHAIN ->BITCOIN

--- src/policy/stake.cpp
+++ src/policy/stake.cpp
@@ -14,66 +14,6 @@
 #include <stdio.h>
 #include <key_io.h>
 
-bool IsProofOfWork(const CBlockHeader& block)
-{
-    return (!block.IsNull() && block.nNonce > 0 && block.nNonce < 0xFFFFFFFF);
-}
-
-bool IsProofOfStake(const CBlockHeader& block)
-{
-    return (!block.IsNull() && block.nNonce == 0);
-}
-
-bool IsPayToYourselfTx(const CTransaction& tx)
-{
-    if (tx.IsNull() || tx.vin.size() != 1 || tx.vout.size() != 1)
-        return false;
-    txnouttype type;
-    std::vector<std::vector<unsigned char>> solutions;
-
-    if (!Solver(tx.vout[0].scriptPubKey, type, solutions))
-        return false;
-    //! TX_PUBKEY is rejected because the public key can not be calculated.
-    if (solutions.size() == 0 || type == TX_PUBKEY || type == TX_NULL_DATA || type == TX_WITNESS_UNKNOWN)
-        return false;
-
-    //! TX_MULTISIG is rejected because the destination can be changed.
-    if (solutions.size() > 1 || type == TX_MULTISIG)
-        return false;
-
-    //! Reject if signature type is different.
-    if ((type == TX_WITNESS_V0_KEYHASH || type == TX_WITNESS_V0_SCRIPTHASH) != tx.HasWitness())
-        return false;
-
-    std::vector<unsigned char> destination;
-
-    if (type == TX_PUBKEYHASH || type == TX_SCRIPTHASH) {
-        std::vector<std::vector<unsigned char>> stack;
-        if (!EvalScript(stack, tx.vin[0].scriptSig, SCRIPT_VERIFY_NONE, BaseSignatureChecker(), SigVersion::BASE))
-            return false;
-        destination = stack.back();
-    } else if (type == TX_WITNESS_V0_KEYHASH || type == TX_WITNESS_V0_SCRIPTHASH) {
-        destination = tx.vin[0].scriptWitness.stack.back();
-    } else {
-        return false;
-    }
-    if (destination.size() == 0)
-        return false;
-    if (type == TX_PUBKEYHASH || type == TX_WITNESS_V0_KEYHASH) {
-        CPubKey pubkey(destination);
-        if (!pubkey.IsFullyValid())
-            return false;
-    }
-    std::vector<unsigned char> hash;
-
-    if (type == TX_WITNESS_V0_SCRIPTHASH) {
-        CSHA256().Write(destination.data(), destination.size()).Finalize(hash.data());
-    } else {
-        CHash160().Write(destination.data(), destination.size()).Finalize(hash.data());
-    }
-    return (hash == solutions[0]);
-}
-
 bool AddressesEqual(const CScript& a, const CScript& b)
 {
     txnouttype aType, bType;
--- src/policy/stake.h
+++ src/policy/stake.h
@@ -2,16 +2,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef XPCHAIN_POLICY_STAKE_H
-#define XPCHAIN_POLICY_STAKE_H
+#ifndef BITCOIN_POLICY_STAKE_H
+#define BITCOIN_POLICY_STAKE_H
 
 #include <primitives/block.h>
 #include <primitives/transaction.h>
 
-bool IsProofOfWork(const CBlockHeader& block);
-bool IsProofOfStake(const CBlockHeader& block);
-
-bool IsPayToYourselfTx(const CTransaction& tx);
 bool AddressesEqual(const CScript& a, const CScript& b);
 
-#endif //XPCHAIN_POLICY_STAKE_H
+#endif //BITCOIN_POLICY_STAKE_H
