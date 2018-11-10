36b76e24d00c97e21a9aec4b494ff9d14ba9204b
serisia <serisia.gma@gmail.com>
2018-11-10 15:46:24 +0900

    Bitcoin network -> XPChain network

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -732,7 +732,7 @@ void BitcoinGUI::updateNetworkState()
     QString tooltip;
 
     if (m_node.getNetworkActive()) {
-        tooltip = tr("%n active connection(s) to Bitcoin network", "", count) + QString(".<br>") + tr("Click to disable network activity.");
+        tooltip = tr("%n active connection(s) to XPChain network", "", count) + QString(".<br>") + tr("Click to disable network activity.");
     } else {
         tooltip = tr("Network activity disabled.") + QString("<br>") + tr("Click to enable network activity again.");
         icon = ":/icons/network_disabled";
