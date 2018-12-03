9576673866d2e5168ecdb206fffd667e1297c036
MaySoMusician <maysomusician@gmail.com>
2018-12-03 17:29:14 +0900

    Fix more typo
    Now BITCOIN_QT_STAKINGREWARDSETTINGPAGE_H is correctly defined

--- src/qt/stakingrewardsettingpage.h
+++ src/qt/stakingrewardsettingpage.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_QT_SRAKINGREWARDSETTINGPAGE_H
-#define BITCOIN_QT_SRAKINGREWARDSETTINGPAGE_H
+#ifndef BITCOIN_QT_STAKINGREWARDSETTINGPAGE_H
+#define BITCOIN_QT_STAKINGREWARDSETTINGPAGE_H
 
 #include <QDialog>
 
