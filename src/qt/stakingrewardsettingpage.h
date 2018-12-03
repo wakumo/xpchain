4f0c4839496837eb21d88dde38e05922edae231a
MaySoMusician <maysomusician@gmail.com>
2018-12-03 17:24:31 +0900

    Fix typo in include guard

--- src/qt/stakingrewardsettingpage.h
+++ src/qt/stakingrewardsettingpage.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_QT_SRAKINFREWARDSETTINGPAGE_H
-#define BITCOIN_QT_SRAKINFREWARDSETTINGPAGE_H
+#ifndef BITCOIN_QT_SRAKINGREWARDSETTINGPAGE_H
+#define BITCOIN_QT_SRAKINGREWARDSETTINGPAGE_H
 
 #include <QDialog>
 
@@ -74,4 +74,4 @@ Q_SIGNALS:
     void sendCoins(QString addr);
 };
 
-#endif // BITCOIN_QT_SRAKINFREWARDSETTINGPAGE_H
+#endif // BITCOIN_QT_SRAKINGREWARDSETTINGPAGE_H
