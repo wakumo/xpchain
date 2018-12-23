7e9dc71649d87b7a2d95600eadbb5e0291aa105a
serisia <serisia.gma@gmail.com>
2018-12-23 18:31:14 +0900

    add seed setting

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -134,7 +134,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("dnsseed.main.xpchain.host");
+        vSeeds.emplace_back("seed1.xpchain.io");
+        vSeeds.emplace_back("seed2.xpchain.io");
+        vSeeds.emplace_back("seed3.xpchain.io");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,76);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,28);
