ed0de411fe0267af5b92cdab85ca676d0a67f3cc
serisia <serisia.gma@gmail.com>
2019-02-17 01:14:33 +0900

    fix TraceThread

--- src/util.h
+++ src/util.h
@@ -322,7 +322,7 @@ void RenameThread(const char* name);
  */
 template <typename Callable> void TraceThread(const char* name,  Callable func)
 {
-    std::string s = strprintf("bitcoin-%s", name);
+    std::string s = strprintf("xpchain-%s", name);
     RenameThread(s.c_str());
     try
     {
